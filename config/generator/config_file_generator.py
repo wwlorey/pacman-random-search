6b9f24c599ab963e5e24feebb306243990366a60
William Lorey <wwlorey@gmail.com>
2018-10-31 15:45:17 -0500

    Add config file generator
    Run tests for small(ish) worlds

---
+++ config/generator/config_file_generator.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+config_template = '[DEFAULT]\n\
+\n\
+###################################\n\
+# GP Parameters\n\
+###################################\n\
+num experiment runs = 3\n\
+num fitness evals = 2000\n\
+\n\
+###################################\n\
+# World Generation Parameters\n\
+###################################\n\
+width = {width}\n\
+height = {height}\n\
+pill density = {pill density}\n\
+wall density = {wall density}\n\
+num ghosts = 3\n\
+\n\
+use external seed = False\n\
+default seed = 123456789\n\
+\n\
+###################################\n\
+# Score \n\
+###################################\n\
+fruit spawn prob = {fruit spawn prob}\n\
+fruit score = {fruit score}\n\
+\n\
+###################################\n\
+# Timing\n\
+###################################\n\
+time multiplier = {time multiplier}\n\
+\n\
+###################################\n\
+# Output Files\n\
+###################################\n\
+log file path = output/{experiment name}_log.txt\n\
+world file path = output/highest_score_game_sequence_all_time_step_world_file_{experiment name}.txt'
+
+config_data = \
+[
+    (10, 10, 50, 25, 0.05, 10, 2, 'small'), 
+    (30, 20, 20, 30, 0.05, 10, 2, 'med_small'), 
+    (40, 50, 70, 35, 0.05, 10, 2, 'med_large'), 
+    (80, 80, 30, 30, 0.05, 10, 2, 'large')
+]
+
+config_data_key = ['width', 'height', 'pill density', 'wall density', 'fruit spawn prob', 'fruit score', 'time multiplier', 'experiment name']
+
+
+# Populate new config files
+for tup in config_data:
+    config_str = config_template
+
+    for i in range(len(tup)):
+        search_str = '{' + config_data_key[i] + '}'
+
+        insertion_index = config_str.find(search_str)
+
+        while insertion_index >= 0:
+            config_str = config_str[:insertion_index] + str(tup[i]) + config_str[insertion_index + len(search_str):]
+
+            insertion_index = config_str.find(search_str)
+
+    # Create config file
+    file_name = '../' + tup[-1] + '.cfg'
+
+    f = open(file_name, 'w')
+
+    f.write(config_str)
+    f.close()
+
